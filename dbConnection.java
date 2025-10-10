c2029c432e45dafb2f694536974370b52674f140
Jia Ying <jiayingkam0811@gmail.com>
2025-10-11 00:32:21 +0800

    Delete dbConnection.java

--- dbConnection.java
+++
@@ -1,9 +0,0 @@
-import java.sql.*;
-
-public class DBConnection {
-    private static final String URL = "jdbc:sqlite:my_database.db";
-    
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL);
-    }
-}
