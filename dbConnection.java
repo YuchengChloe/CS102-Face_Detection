7886600078a4da96d69263034cad1b38fb68cf51
Jia Ying <jiayingkam0811@gmail.com>
2025-10-11 01:32:07 +0800

    update

--- dbConnection.java
+++ dbConnection.java
@@ -1,7 +1,7 @@
 import java.sql.*;
 
 public class DBConnection {
-    private static final String URL = "jdbc:sqlite:my_database.db";
+    private static final String URL = "jdbc:sqlite:studentDB.db";
     
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL);
